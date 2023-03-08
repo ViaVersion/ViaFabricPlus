f4be4c420543d68f4a1162e7884856615016aebc
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-03-08 22:08:13 +0100

    added License header

--- src/main/java/de/florianmichael/viafabricplus/ViaFabricPlus.java
+++ src/main/java/de/florianmichael/viafabricplus/ViaFabricPlus.java
@@ -1,6 +1,22 @@
+/*
+ * This file is part of ViaFabricPlus - https://github.com/FlorianMichael/ViaFabricPlus
+ * Copyright (C) 2023 FlorianMichael/EnZaXD and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.florianmichael.viafabricplus;
 
-import com.viaversion.viaversion.api.Via;
 import com.viaversion.viaversion.api.connection.UserConnection;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import com.viaversion.viaversion.protocols.protocol1_9to1_8.providers.HandItemProvider;
@@ -18,7 +34,6 @@ import de.florianmichael.viafabricplus.settings.SettingGroup;
 import de.florianmichael.viafabricplus.settings.groups.*;
 import de.florianmichael.viafabricplus.util.SettingsSave;
 import de.florianmichael.vialoadingbase.ViaLoadingBase;
-import de.florianmichael.vialoadingbase.platform.InternalProtocolList;
 import de.florianmichael.vialoadingbase.platform.SubPlatform;
 import io.netty.channel.DefaultEventLoop;
 import io.netty.util.AttributeKey;
