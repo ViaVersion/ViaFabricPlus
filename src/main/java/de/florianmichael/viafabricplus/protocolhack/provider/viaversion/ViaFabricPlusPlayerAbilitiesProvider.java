1b9fd0a12f668f88808e94d9fd2e79acd20c94e3
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-05-04 15:45:59 +0200

    Updated Via* (23w18a)

--- src/main/java/de/florianmichael/viafabricplus/protocolhack/provider/viaversion/ViaFabricPlusPlayerAbilitiesProvider.java
+++ src/main/java/de/florianmichael/viafabricplus/protocolhack/provider/viaversion/ViaFabricPlusPlayerAbilitiesProvider.java
@@ -17,18 +17,19 @@
  */
 package de.florianmichael.viafabricplus.protocolhack.provider.viaversion;
 
+import com.viaversion.viaversion.api.connection.UserConnection;
 import com.viaversion.viaversion.protocols.protocol1_16to1_15_2.provider.PlayerAbilitiesProvider;
 import net.minecraft.client.MinecraftClient;
 
 public class ViaFabricPlusPlayerAbilitiesProvider extends PlayerAbilitiesProvider {
 
     @Override
-    public float getFlyingSpeed() {
+    public float getFlyingSpeed(UserConnection connection) {
         return MinecraftClient.getInstance().player.getAbilities().getFlySpeed();
     }
 
     @Override
-    public float getWalkingSpeed() {
+    public float getWalkingSpeed(UserConnection connection) {
         return MinecraftClient.getInstance().player.getAbilities().getWalkSpeed();
     }
 }
