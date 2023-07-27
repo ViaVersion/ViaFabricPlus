afbb9862401d81c515f06ef18bcf941e960e1fd6
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-07-28 01:11:01 +0200

    Fixed https://github.com/ViaVersion/ViaFabricPlus/issues/197

---
+++ src/main/java/de/florianmichael/viafabricplus/injection/mixin/fixes/minecraft/block/MixinCropBlock.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of ViaFabricPlus - https://github.com/FlorianMichael/ViaFabricPlus
+ * Copyright (C) 2021-2023 FlorianMichael/EnZaXD and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.florianmichael.viafabricplus.injection.mixin.fixes.minecraft.block;
+
+import de.florianmichael.viafabricplus.protocolhack.ProtocolHack;
+import net.minecraft.block.Block;
+import net.minecraft.block.CropBlock;
+import net.minecraft.util.shape.VoxelShape;
+import net.raphimc.vialoader.util.VersionEnum;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CropBlock.class)
+public class MixinCropBlock {
+
+    @Unique
+    private final VoxelShape viafabricplus_shape_v1_8_x = Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 4.0, 16.0);
+
+    @Inject(method = "getOutlineShape", at = @At("HEAD"), cancellable = true)
+    public void injectGetOutlineShape(CallbackInfoReturnable<VoxelShape> cir) {
+        if (ProtocolHack.getTargetVersion().isOlderThanOrEqualTo(VersionEnum.r1_8)) {
+            cir.setReturnValue(viafabricplus_shape_v1_8_x);
+        }
+    }
+}
