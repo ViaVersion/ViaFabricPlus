cd5a4f39aac3714e53b8a690e049bc947cf920ac
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-08-24 17:53:49 +0200

    Track movement related TODOs

--- src/main/java/de/florianmichael/viafabricplus/ViaFabricPlus.java
+++ src/main/java/de/florianmichael/viafabricplus/ViaFabricPlus.java
@@ -41,16 +41,20 @@ import org.apache.logging.log4j.Logger;
 import java.io.File;
 
 /*
- * TODO | ViaFabricPlus
+ * TODO | General
  *  - Check if relevant for protocol translation: TakeItemEntityPacket isEmpty case (1.20 -> 1.20.1 change)
- *  - Cobwebs in <= b1.7.3 are broken (movement has been changed)
  *  - Window interactions in <= 1.16.5 has changed and can be detected by the server
  *  - Entity hit boxes and eye heights has changed in almost all versions
  *  - Crafting Recipes are missing in ViaVersion (see https://github.com/ViaVersion/ViaFabricPlus/issues/60)
- *  - Various movement related changes (see https://github.com/ViaVersion/ViaFabricPlus/issues/189 and https://github.com/ViaVersion/ViaFabricPlus/issues/195)
  *  - Most CTS protocol features aren't supported (see https://github.com/ViaVersion/ViaFabricPlus/issues/181)
  *  - Most CPE features aren't implemented correctly (see https://github.com/ViaVersion/ViaFabricPlus/issues/152)
  *  - Bedrock scaffolding should be added as soon as ViaBedrock supports block placement (see https://github.com/ViaVersion/ViaFabricPlus/issues/204)
+ *
+ * TODO | Movement
+ *  - Cobwebs in <= b1.7.3 are broken (movement has been changed)
+ *  - X/Z Face based jump movement in <= 1.13.2 is broken (https://github.com/ViaVersion/ViaFabricPlus/issues/189)
+ *  - Collision hit boxes has been changed (https://github.com/ViaVersion/ViaFabricPlus/issues/195)
+ *  - Blit-jump is not supported in <= 1.8.9 (https://github.com/ViaVersion/ViaFabricPlus/issues/225)
  */
 public class ViaFabricPlus {
     public final static Gson GSON = new GsonBuilder().setPrettyPrinting().create();
