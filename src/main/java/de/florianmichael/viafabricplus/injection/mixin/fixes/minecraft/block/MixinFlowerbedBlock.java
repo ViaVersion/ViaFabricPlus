e02ca49137e4beb6d5ae253261fbbc4504efee96
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-09-23 17:45:06 +0200

    Fixed PinkPetals bounding box in <= 1.20.1

---
+++ src/main/java/de/florianmichael/viafabricplus/injection/mixin/fixes/minecraft/block/MixinFlowerbedBlock.java
@@ -0,0 +1,47 @@
+/*
+ * This file is part of ViaFabricPlus - https://github.com/FlorianMichael/ViaFabricPlus
+ * Copyright (C) 2021-2023 FlorianMichael/EnZaXD and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.florianmichael.viafabricplus.injection.mixin.fixes.minecraft.block;
+
+import de.florianmichael.viafabricplus.protocolhack.ProtocolHack;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FlowerbedBlock;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
+import net.raphimc.vialoader.util.VersionEnum;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FlowerbedBlock.class)
+public class MixinFlowerbedBlock {
+
+    @Unique
+    private final static VoxelShape viafabricplus_shape_v1_20_1 = VoxelShapes.cuboid(0.0, 0.0, 0.0, 16.0, 3.0, 16.0);
+
+    @Inject(method = "getOutlineShape", at = @At("HEAD"), cancellable = true)
+    public void replaceShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir) {
+        if (ProtocolHack.getTargetVersion().isOlderThanOrEqualTo(VersionEnum.r1_20tor1_20_1)) {
+            cir.setReturnValue(viafabricplus_shape_v1_20_1);
+        }
+    }
+}
