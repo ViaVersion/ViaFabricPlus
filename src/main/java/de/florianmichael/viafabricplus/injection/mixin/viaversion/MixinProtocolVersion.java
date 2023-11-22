7cb50c76d2a074875e4c2ef3c3ebbc4f059d588e
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-11-22 18:10:19 +0100

    1.20.3-pre2

--- src/main/java/de/florianmichael/viafabricplus/injection/mixin/viaversion/MixinProtocolVersion.java
+++ src/main/java/de/florianmichael/viafabricplus/injection/mixin/viaversion/MixinProtocolVersion.java
@@ -46,7 +46,7 @@ public abstract class MixinProtocolVersion {
         viafabricplus_remaps.put("1.18/1.18.1", new Pair<>("1.18-1.18.1", null));
         viafabricplus_remaps.put("1.19.1/2", new Pair<>("1.19.1-1.19.2", null));
         viafabricplus_remaps.put("1.20/1.20.1", new Pair<>("1.20-1.20.1", null));
-        viafabricplus_remaps.put("1.20.3", new Pair<>("1.20.3-pre1", null));
+        viafabricplus_remaps.put("1.20.3", new Pair<>("1.20.3-pre2", null));
     }
 
     @Redirect(method = "<clinit>", at = @At(value = "INVOKE", target = "Lcom/viaversion/viaversion/api/protocol/version/ProtocolVersion;register(ILjava/lang/String;)Lcom/viaversion/viaversion/api/protocol/version/ProtocolVersion;"))
