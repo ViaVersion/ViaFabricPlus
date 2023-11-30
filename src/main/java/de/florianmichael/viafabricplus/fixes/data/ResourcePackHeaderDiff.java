a0f233db30ce30af69a49f9bdabf72ef11b042d8
RaphiMC <50594595+RaphiMC@users.noreply.github.com>
2023-11-30 17:33:48 +0100

    1.20.3-rc1

--- src/main/java/de/florianmichael/viafabricplus/fixes/data/ResourcePackHeaderDiff.java
+++ src/main/java/de/florianmichael/viafabricplus/fixes/data/ResourcePackHeaderDiff.java
@@ -35,7 +35,7 @@ public class ResourcePackHeaderDiff {
     private final static Map<VersionEnum, GameVersion> GAME_VERSION_DIFF = new HashMap<>();
 
     static {
-        registerVersion(VersionEnum.r1_20_3, 22, "1.20.3-pre4");
+        registerVersion(VersionEnum.r1_20_3, 22, "1.20.3-rc1");
         registerVersion(VersionEnum.r1_20_2, 18, "1.20.2");
         registerVersion(VersionEnum.r1_20tor1_20_1, 15, "1.20.1"); // 1.20 and 1.20.1 are equal
         registerVersion(VersionEnum.r1_19_4, 13, "1.19.4");
