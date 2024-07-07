6d4bb61d470da8d50b887120cb610903d8808d4f
FlorianMichael <florian.michael07@gmail.com>
2024-07-07 16:37:15 +0200

    Apply our chat screen mixin before everyone else
    Allows other mods to modify the chat field length without changing their priority.

--- src/main/java/de/florianmichael/viafabricplus/injection/mixin/fixes/minecraft/screen/MixinChatScreen.java
+++ src/main/java/de/florianmichael/viafabricplus/injection/mixin/fixes/minecraft/screen/MixinChatScreen.java
@@ -36,7 +36,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ChatScreen.class)
+@Mixin(value = ChatScreen.class, priority = 1) // Apply our mixin first so other mods can override the chat length
 public abstract class MixinChatScreen {
 
     @Shadow
