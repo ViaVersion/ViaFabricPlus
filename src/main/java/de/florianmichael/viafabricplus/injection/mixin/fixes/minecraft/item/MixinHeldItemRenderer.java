8119de3b4ed0cb84e1dd456bd146ce9d66822606
FlorianMichael <florian.michael07@gmail.com>
2024-08-20 06:59:04 +0200

    Fix formatting

--- src/main/java/de/florianmichael/viafabricplus/injection/mixin/fixes/minecraft/item/MixinHeldItemRenderer.java
+++ src/main/java/de/florianmichael/viafabricplus/injection/mixin/fixes/minecraft/item/MixinHeldItemRenderer.java
@@ -37,31 +37,31 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HeldItemRenderer.class)
 public abstract class MixinHeldItemRenderer {
-	
-	@Shadow
-	protected abstract void applySwingOffset(MatrixStack matrices, Arm arm, float swingProgress);
-	
-	@Inject(method = "renderFirstPersonItem",
-			slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getUseAction()Lnet/minecraft/util/UseAction;")),
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/HeldItemRenderer;applyEquipOffset(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/Arm;F)V", ordinal = 2, shift = At.Shift.AFTER))
-	private void transformSwordBlockingPosition(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
-		final boolean blockHitAnimation = VisualSettings.global().enableBlockHitAnimation.isEnabled();
-		if (!VisualSettings.global().enableSwordBlocking.isEnabled() && !blockHitAnimation) {
-			return;
-		}
-		
-		final Arm arm = hand == Hand.MAIN_HAND ? player.getMainArm() : player.getMainArm().getOpposite();
-		final int direction = arm == Arm.RIGHT ? 1 : -1;
-		
-		if (blockHitAnimation) {
-			applySwingOffset(matrices, arm, swingProgress);
-		}
-		
-		// Values stripped from early 1.9 snapshots, 15w33b specifically, which is the version prior to them removing sword blocking
-		matrices.translate(direction * -0.14142136F, 0.08F, 0.14142136F);
-		matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(-102.25F));
-		matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(direction * 13.365F));
-		matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(direction * 78.05F));
-	}
-	
+
+    @Shadow
+    protected abstract void applySwingOffset(MatrixStack matrices, Arm arm, float swingProgress);
+
+    @Inject(method = "renderFirstPersonItem",
+            slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getUseAction()Lnet/minecraft/util/UseAction;")),
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/HeldItemRenderer;applyEquipOffset(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/Arm;F)V", ordinal = 2, shift = At.Shift.AFTER))
+    private void transformSwordBlockingPosition(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand, float swingProgress, ItemStack item, float equipProgress, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        final boolean blockHitAnimation = VisualSettings.global().enableBlockHitAnimation.isEnabled();
+        if (!VisualSettings.global().enableSwordBlocking.isEnabled() && !blockHitAnimation) {
+            return;
+        }
+
+        final Arm arm = hand == Hand.MAIN_HAND ? player.getMainArm() : player.getMainArm().getOpposite();
+        final int direction = arm == Arm.RIGHT ? 1 : -1;
+
+        if (blockHitAnimation) {
+            applySwingOffset(matrices, arm, swingProgress);
+        }
+
+        // Values stripped from early 1.9 snapshots, 15w33b specifically, which is the version prior to them removing sword blocking
+        matrices.translate(direction * -0.14142136F, 0.08F, 0.14142136F);
+        matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(-102.25F));
+        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(direction * 13.365F));
+        matrices.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(direction * 78.05F));
+    }
+
 }
