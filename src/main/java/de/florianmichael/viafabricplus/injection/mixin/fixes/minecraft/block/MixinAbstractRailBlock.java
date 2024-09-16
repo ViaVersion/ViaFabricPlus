59833371d35449f6ac74f034db0204138d76a968
FlorianMichael <florian.michael07@gmail.com>
2024-09-16 17:25:38 +0200

    Cleanup, use Redirect instead

--- src/main/java/de/florianmichael/viafabricplus/injection/mixin/fixes/minecraft/block/MixinAbstractRailBlock.java
+++ src/main/java/de/florianmichael/viafabricplus/injection/mixin/fixes/minecraft/block/MixinAbstractRailBlock.java
@@ -22,38 +22,37 @@ package de.florianmichael.viafabricplus.injection.mixin.fixes.minecraft.block;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import de.florianmichael.viafabricplus.protocoltranslator.ProtocolTranslator;
 import net.minecraft.block.*;
-import net.minecraft.block.enums.RailShape;
-import net.minecraft.state.property.Property;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.world.BlockView;
+import net.minecraft.util.shape.VoxelShapes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(AbstractRailBlock.class)
 public abstract class MixinAbstractRailBlock extends Block {
+
+    @Shadow
+    @Final
+    protected static VoxelShape ASCENDING_SHAPE;
+
+    @Unique
+    private static final VoxelShape viaFabricPlus$ascending_shape_r1_10_x = VoxelShapes.fullCube();
 
     public MixinAbstractRailBlock(Settings settings) {
         super(settings);
     }
 
-    @Shadow public abstract Property<RailShape> getShapeProperty();
-
-    @Unique
-    private static final VoxelShape viaFabricPlus$ascending_shape_r1_10_x = Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
-
-    @Inject(method = "getOutlineShape", at = @At("HEAD"), cancellable = true)
-    private void changeOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir) {
+    @Redirect(method = "getOutlineShape", at = @At(value = "FIELD", target = "Lnet/minecraft/block/AbstractRailBlock;ASCENDING_SHAPE:Lnet/minecraft/util/shape/VoxelShape;"))
+    private VoxelShape changeOutlineShape() {
         // https://bugs.mojang.com/browse/MC-102638
         if (ProtocolTranslator.getTargetVersion().equalTo(ProtocolVersion.v1_10)) {
-            final RailShape shape = state.isOf(this) ? state.get(getShapeProperty()) : null;
-            if (shape != null && shape.isAscending()) {
-                cir.setReturnValue(viaFabricPlus$ascending_shape_r1_10_x);
-            }
+            return viaFabricPlus$ascending_shape_r1_10_x;
+        } else {
+            return ASCENDING_SHAPE;
         }
     }
+
 }
