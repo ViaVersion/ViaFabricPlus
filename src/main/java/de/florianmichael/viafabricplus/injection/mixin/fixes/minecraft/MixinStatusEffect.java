5edf290a030c04149b3687f0b47359f0ed9bb8eb
FlorianMichael <florian.michael07@gmail.com>
2024-09-23 21:16:30 +0200

    Fix

--- src/main/java/de/florianmichael/viafabricplus/injection/mixin/fixes/minecraft/MixinStatusEffect.java
+++ src/main/java/de/florianmichael/viafabricplus/injection/mixin/fixes/minecraft/MixinStatusEffect.java
@@ -23,17 +23,20 @@ import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import de.florianmichael.viafabricplus.protocoltranslator.ProtocolTranslator;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.util.math.MathHelper;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(StatusEffectInstance.class)
-public class MixinStatusEffect {
+public abstract class MixinStatusEffect {
 
-    @ModifyArg(method = "<init>(Lnet/minecraft/registry/entry/RegistryEntry;IIZZZLnet/minecraft/entity/effect/StatusEffectInstance;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;clamp(III)I"), index = 1)
-    public int clamp(int min) {
-        return ProtocolTranslator.getTargetVersion().olderThanOrEqualTo(ProtocolVersion.v1_20_3) ? -255 : 0;
+    @Redirect(method = "<init>(Lnet/minecraft/registry/entry/RegistryEntry;IIZZZLnet/minecraft/entity/effect/StatusEffectInstance;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;clamp(III)I"))
+    private int dontClampValue(int value, int min, int max) {
+        if (ProtocolTranslator.getTargetVersion().olderThanOrEqualTo(ProtocolVersion.v1_20_3)) {
+            return value;
+        } else {
+            return MathHelper.clamp(value, min, max);
+        }
     }
+
 }
