c152d7b2bb4f69ba9b6b69da8f4494e380e33e18
lowercasebtw <jejojora2@gmail.com>
2024-12-07 16:37:07 -0500

    Add toggle/setting for reverting villager skins to how they looked prior to 1.14

--- src/main/java/de/florianmichael/viafabricplus/injection/mixin/fixes/minecraft/entity/MixinVillagerClothingFeatureRenderer.java
+++ src/main/java/de/florianmichael/viafabricplus/injection/mixin/fixes/minecraft/entity/MixinVillagerClothingFeatureRenderer.java
@@ -21,6 +21,7 @@ package de.florianmichael.viafabricplus.injection.mixin.fixes.minecraft.entity;
 
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import de.florianmichael.viafabricplus.protocoltranslator.ProtocolTranslator;
+import de.florianmichael.viafabricplus.settings.impl.VisualSettings;
 import net.minecraft.client.render.entity.feature.VillagerClothingFeatureRenderer;
 import net.minecraft.village.VillagerData;
 import net.minecraft.village.VillagerProfession;
@@ -33,7 +34,7 @@ public abstract class MixinVillagerClothingFeatureRenderer {
 
     @Redirect(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/client/render/entity/state/LivingEntityRenderState;FF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/village/VillagerData;getProfession()Lnet/minecraft/village/VillagerProfession;"))
     private VillagerProfession revertVillagerVisual(VillagerData instance) {
-        if (ProtocolTranslator.getTargetVersion().olderThanOrEqualTo(ProtocolVersion.v1_13_2)) {
+        if (VisualSettings.global().revertVillagerSkins.getValue()) {
             return VillagerProfession.NONE;
         } else {
             return instance.getProfession();
