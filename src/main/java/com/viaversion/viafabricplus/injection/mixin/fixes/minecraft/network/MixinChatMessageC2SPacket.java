64d1ffaff89f7c5900cd59de65b693b11a6e13d6
FlorianMichael <florian.michael07@gmail.com>
2024-12-23 07:19:14 +0100

    Update license header

--- src/main/java/com/viaversion/viafabricplus/injection/mixin/fixes/minecraft/network/MixinChatMessageC2SPacket.java
+++ src/main/java/com/viaversion/viafabricplus/injection/mixin/fixes/minecraft/network/MixinChatMessageC2SPacket.java
@@ -1,7 +1,9 @@
 /*
- * This file is part of ViaFabricPlus - https://github.com/FlorianMichael/ViaFabricPlus
- * Copyright (C) 2021-2024 FlorianMichael/EnZaXD <florian.michael07@gmail.com> and RK_01/RaphiMC
- * Copyright (C) 2023-2024 contributors
+ * This file is part of ViaFabricPlus - https://github.com/ViaVersion/ViaFabricPlus
+ * Copyright (C) 2021-2024 the original authors
+ *                         - FlorianMichael/EnZaXD <florian.michael07@gmail.com>
+ *                         - RK_01/RaphiMC
+ * Copyright (C) 2023-2024 ViaVersion and contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
