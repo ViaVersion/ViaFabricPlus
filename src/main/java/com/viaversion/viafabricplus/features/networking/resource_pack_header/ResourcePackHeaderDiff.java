043acf51af18d18513449083384e41a2cb8e4719
FlorianMichael <florian.michael07@gmail.com>
2025-09-30 17:56:27 +0200

    Move resource pack headers

--- src/main/java/com/viaversion/viafabricplus/features/networking/resource_pack_header/ResourcePackHeaderDiff.java
+++ src/main/java/com/viaversion/viafabricplus/features/networking/resource_pack_header/ResourcePackHeaderDiff.java
@@ -21,7 +21,9 @@
 
 package com.viaversion.viafabricplus.features.networking.resource_pack_header;
 
+import com.viaversion.viafabricplus.protocoltranslator.impl.ViaFabricPlusMappingDataLoader;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import com.viaversion.viaversion.libs.gson.JsonObject;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -37,58 +39,18 @@ public final class ResourcePackHeaderDiff {
 
     private final static Map<ProtocolVersion, GameVersion> GAME_VERSION_DIFF = new HashMap<>();
 
-    static {
-        registerVersion(ProtocolVersion.v1_21_7, 64, "1.21.8");
-        registerVersion(ProtocolVersion.v1_21_6, 63, "1.21.6");
-        registerVersion(ProtocolVersion.v1_21_5, 55, "1.21.5");
-        registerVersion(ProtocolVersion.v1_21_4, 46, "1.21.4");
-        registerVersion(ProtocolVersion.v1_21_2, 42, "1.21.3");
-        registerVersion(ProtocolVersion.v1_21, 34, "1.21.1");
-        registerVersion(ProtocolVersion.v1_20_5, 32, "1.20.6");
-        registerVersion(ProtocolVersion.v1_20_3, 22, "1.20.4");
-        registerVersion(ProtocolVersion.v1_20_2, 18, "1.20.2");
-        registerVersion(ProtocolVersion.v1_20, 15, "1.20.1");
-        registerVersion(ProtocolVersion.v1_19_4, 13, "1.19.4");
-        registerVersion(ProtocolVersion.v1_19_3, 12, "1.19.3");
-        registerVersion(ProtocolVersion.v1_19_1, 9, "1.19.2");
-        registerVersion(ProtocolVersion.v1_19, 9, "1.19");
-        registerVersion(ProtocolVersion.v1_18_2, 8, "1.18.2");
-        registerVersion(ProtocolVersion.v1_18, 8, "1.18.1");
-        registerVersion(ProtocolVersion.v1_17_1, 7, "1.17.1");
-        registerVersion(ProtocolVersion.v1_17, 7, "1.17");
-        registerVersion(ProtocolVersion.v1_16_4, 6, "1.16.5");
-        registerVersion(ProtocolVersion.v1_16_3, 6, "1.16.3");
-        registerVersion(ProtocolVersion.v1_16_2, 6, "1.16.2");
-        registerVersion(ProtocolVersion.v1_16_1, 5, "1.16.1");
-        registerVersion(ProtocolVersion.v1_16, 5, "1.16");
-        registerVersion(ProtocolVersion.v1_15_2, 5, "1.15.2");
-        registerVersion(ProtocolVersion.v1_15_1, 5, "1.15.1");
-        registerVersion(ProtocolVersion.v1_15, 5, "1.15");
-        registerVersion(ProtocolVersion.v1_14_4, 4, "1.14.4");
-        registerVersion(ProtocolVersion.v1_14_3, 4, "1.14.3");
-        registerVersion(ProtocolVersion.v1_14_2, 4, "1.14.2");
-        registerVersion(ProtocolVersion.v1_14_1, 4, "1.14.1");
-        registerVersion(ProtocolVersion.v1_14, 4, "1.14");
-        registerVersion(ProtocolVersion.v1_13_2, 4, "1.13.2");
-        registerVersion(ProtocolVersion.v1_13_1, 4, "1.13.1");
-        registerVersion(ProtocolVersion.v1_13, 4, "1.13");
-        registerVersion(ProtocolVersion.v1_12_2, 3, "1.12.2");
-        registerVersion(ProtocolVersion.v1_12_1, 3, "1.12.1");
-        registerVersion(ProtocolVersion.v1_12, 3, "1.12");
-        registerVersion(ProtocolVersion.v1_11_1, 3, "1.11.2");
-        registerVersion(ProtocolVersion.v1_11, 3, "1.11");
-        registerVersion(ProtocolVersion.v1_10, 2, "1.10.2");
-        registerVersion(ProtocolVersion.v1_9_3, 2, "1.9.4");
-        registerVersion(ProtocolVersion.v1_9_2, 2, "1.9.2");
-        registerVersion(ProtocolVersion.v1_9_1, 2, "1.9.1");
-        registerVersion(ProtocolVersion.v1_9, 2, "1.9");
-        registerVersion(ProtocolVersion.v1_8, 1, "1.8.9");
-        registerVersion(ProtocolVersion.v1_7_6, 1, "1.7.10");
-        registerVersion(ProtocolVersion.v1_7_2, 1, "1.7.5");
-    }
-
     public static void init() {
-        // Also calls the static block
+        if (!GAME_VERSION_DIFF.isEmpty()) {
+            throw new IllegalStateException("ResourcePackHeaderDiff is already initialized");
+        }
+
+        final JsonObject diff = ViaFabricPlusMappingDataLoader.INSTANCE.loadData("resource-pack-headers.json");
+        for (final String string : diff.keySet()) {
+            final JsonObject data = diff.getAsJsonObject(string);
+            final int version = data.get("version").getAsInt();
+            final int packFormat = data.get("pack_format").getAsInt();
+            registerVersion(ProtocolVersion.getProtocol(version), packFormat, string, string);
+        }
     }
 
     /**
@@ -101,10 +63,6 @@ public final class ResourcePackHeaderDiff {
         } else {
             return GAME_VERSION_DIFF.get(version);
         }
-    }
-
-    private static void registerVersion(final ProtocolVersion version, final int packFormat, final String name) {
-        registerVersion(version, packFormat, name, name);
     }
 
     private static void registerVersion(final ProtocolVersion version, final int packFormat, final String name, final String id) {
