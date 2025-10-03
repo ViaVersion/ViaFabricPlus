6f938ec3b467bed3497c376dd3cda52a8c6e2e93
FlorianMichael <florian.michael07@gmail.com>
2025-10-03 16:06:15 +0200

    Disable maven local again

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ allprojects {
             }
         }
 
-        mavenLocal() // Uncomment during Minecraft updates for preview VV/VB builds
+        //mavenLocal() // Uncomment during Minecraft updates for preview VV/VB builds
     }
 
 }
