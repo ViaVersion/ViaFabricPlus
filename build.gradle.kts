59c04b6fba0c3e4892bab72ea0c2df1d953f8abe
FlorianMichael <florian.michael07@gmail.com>
2025-10-05 16:10:15 +0200

    Update ViaAprilFools

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,7 @@ fun Project.configureVVDependencies(configuration: String) {
     dependencies {
         configuration("com.viaversion:viaversion-common:5.5.1-SNAPSHOT")
         configuration("com.viaversion:viabackwards-common:5.5.1-SNAPSHOT")
-        configuration("com.viaversion:viaaprilfools-common:4.0.4")
+        configuration("com.viaversion:viaaprilfools-common:4.0.5-SNAPSHOT")
         configuration("com.viaversion:vialoader:4.0.5") {
             exclude(group = "com.google.guava", module = "guava")
             exclude(group = "org.slf4j", module = "slf4j-api")
