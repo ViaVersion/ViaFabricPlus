4d34a1ff721d6182c9fe07dff9bd5ff36a963344
Ryan <87820585+SprainedSpark89@users.noreply.github.com>
2025-11-08 10:03:34 -0800

    even less comments
    wasn't expecting someone to look

--- viafabricplus-visuals/src/main/java/com/viaversion/viafabricplus/visuals/injection/mixin/classic/walking_animation/MixinLivingEntityRenderer.java
+++ viafabricplus-visuals/src/main/java/com/viaversion/viafabricplus/visuals/injection/mixin/classic/walking_animation/MixinLivingEntityRenderer.java
@@ -1,4 +1,4 @@
-package com.viaversion.viafabricplus.visuals.injection.mixin.classic.walking_animation; // declares the current package
+package com.viaversion.viafabricplus.visuals.injection.mixin.classic.walking_animation;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
