164c1363ebad5f4053656232a44c7268febaf7dd
lowercasebtw <ignzoudy@gmail.com>
2025-12-15 01:25:48 -0500

    Fix epsilon in block intersection for <=1.21.8

---
+++ src/main/java/com/viaversion/viafabricplus/injection/mixin/features/movement/collision/MixinBlockGetter.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of ViaFabricPlus - https://github.com/ViaVersion/ViaFabricPlus
+ * Copyright (C) 2021-2025 the original authors
+ *                         - FlorianMichael/EnZaXD <florian.michael07@gmail.com>
+ *                         - RK_01/RaphiMC
+ * Copyright (C) 2023-2025 ViaVersion and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.viaversion.viafabricplus.injection.mixin.features.movement.collision;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.viaversion.viafabricplus.protocoltranslator.ProtocolTranslator;
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import net.minecraft.world.level.BlockGetter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(BlockGetter.class)
+public interface MixinBlockGetter {
+
+    @ModifyExpressionValue(method = "forEachBlockIntersectedBetween", at = @At(value = "CONSTANT", args = "floatValue=1.0E-5"))
+    private static float use1_21_8Epsilon(float original) {
+        if (ProtocolTranslator.getTargetVersion().olderThanOrEqualTo(ProtocolVersion.v1_21_7)) {
+            return 0.99999F;
+        } else {
+            return original;
+        }
+    }
+
+}
