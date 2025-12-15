0d98e482b36b52ec5636bfa36d436768e97385a3
lowercasebtw <ignzoudy@gmail.com>
2025-12-15 18:24:02 -0500

    Fix usage mining in <=1.7.x where mining progress wasn't resetting properly

--- src/main/java/com/viaversion/viafabricplus/injection/mixin/features/interaction/replace_block_item_use_logic/MixinMultiPlayerGameMode.java
+++ src/main/java/com/viaversion/viafabricplus/injection/mixin/features/interaction/replace_block_item_use_logic/MixinMultiPlayerGameMode.java
@@ -21,6 +21,7 @@
 
 package com.viaversion.viafabricplus.injection.mixin.features.interaction.replace_block_item_use_logic;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.viaversion.viafabricplus.features.interaction.replace_block_placement_logic.ActionResultException1_12_2;
 import com.viaversion.viafabricplus.protocoltranslator.ProtocolTranslator;
@@ -52,6 +53,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.level.GameType;
 import net.minecraft.world.level.Level;
 import org.apache.commons.lang3.mutable.MutableObject;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
@@ -88,6 +90,9 @@ public abstract class MixinMultiPlayerGameMode {
 
     @Shadow
     private GameType localPlayerMode;
+
+    @Shadow
+    private boolean isDestroying;
 
     @Redirect(method = "performUseItemOn", at = @At(value = "FIELD", target = "Lnet/minecraft/world/InteractionResult;CONSUME:Lnet/minecraft/world/InteractionResult$Success;"))
     private InteractionResult.Success changeSpectatorAction() {
@@ -238,6 +243,16 @@ public abstract class MixinMultiPlayerGameMode {
             this.startPrediction(world, packetCreator);
         } catch (ActionResultException1_12_2 ignored) {
         }
+    }
+
+    @Redirect(method = "stopDestroyBlock", at = @At(value = "FIELD", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;isDestroying:Z", opcode = Opcodes.GETFIELD))
+    private boolean fixMiningReset1_7(MultiPlayerGameMode instance) {
+        return ProtocolTranslator.getTargetVersion().olderThanOrEqualTo(ProtocolVersion.v1_7_6) || instance.isDestroying();
+    }
+
+    @WrapWithCondition(method = "stopDestroyBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientPacketListener;send(Lnet/minecraft/network/protocol/Packet;)V"))
+    private boolean preventPacketWhenNotMining1_7(ClientPacketListener instance, Packet<?> packet) {
+        return !ProtocolTranslator.getTargetVersion().olderThanOrEqualTo(ProtocolVersion.v1_7_6) || this.isDestroying;
     }
 
     @Unique
