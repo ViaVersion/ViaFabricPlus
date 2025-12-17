2796e37af8269212ed2ec33e340b6cd13835e5de
lowercasebtw <126462578+lowercasebtw@users.noreply.github.com>
2025-12-17 10:41:34 -0800

    Fix lava movement in <= 1.8; Rename feature folder (#974)
    Fixes https://github.com/ViaVersion/ViaFabricPlus/issues/728

--- src/main/java/com/viaversion/viafabricplus/injection/mixin/features/movement/liquid/MixinItemEntity.java
+++ src/main/java/com/viaversion/viafabricplus/injection/mixin/features/movement/liquid/MixinItemEntity.java
@@ -19,30 +19,23 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.viaversion.viafabricplus.injection.mixin.features.movement.water;
+package com.viaversion.viafabricplus.injection.mixin.features.movement.liquid;
 
 import com.viaversion.viafabricplus.protocoltranslator.ProtocolTranslator;
 import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.animal.equine.AbstractHorse;
-import net.minecraft.world.entity.animal.equine.SkeletonHorse;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.item.ItemEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(SkeletonHorse.class)
-public abstract class MixinSkeletonHorse extends AbstractHorse {
+@Mixin(ItemEntity.class)
+public abstract class MixinItemEntity {
 
-    protected MixinSkeletonHorse(EntityType<? extends AbstractHorse> entityType, Level world) {
-        super(entityType, world);
-    }
-
-    @Inject(method = "getWaterSlowDown", at = @At("HEAD"), cancellable = true)
-    private void modifyBaseWaterMovementSpeedMultiplier(CallbackInfoReturnable<Float> cir) {
+    @Inject(method = "setUnderwaterMovement", at = @At("HEAD"), cancellable = true)
+    private void dontApplyWaterBuoyancy(CallbackInfo ci) {
         if (ProtocolTranslator.getTargetVersion().olderThanOrEqualTo(ProtocolVersion.v1_12_2)) {
-            cir.setReturnValue(super.getWaterSlowDown());
+            ci.cancel();
         }
     }
 
