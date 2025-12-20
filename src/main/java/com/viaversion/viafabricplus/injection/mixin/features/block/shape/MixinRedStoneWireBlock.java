42813e0d3c5c1ab041f0a91a3b21ea1d1f3bd0ad
lowercasebtw <126462578+lowercasebtw@users.noreply.github.com>
2025-12-20 13:05:18 -0800

    Fix redstone shape in <= 1.8, disable shape toggling in <= 1.15.2 (#988)
    * Change redstone wire outline shape in <= 1.8.x
    
    * Disable toggling blockstate shape & remove default cross in <= 1.15.2

---
+++ src/main/java/com/viaversion/viafabricplus/injection/mixin/features/block/shape/MixinRedStoneWireBlock.java
@@ -0,0 +1,57 @@
+/*
+ * This file is part of ViaFabricPlus - https://github.com/ViaVersion/ViaFabricPlus
+ * Copyright (C) 2021-2025 the original authors
+ *                         - FlorianMichael/EnZaXD <florian.michael07@gmail.com>
+ *                         - RK_01/RaphiMC
+ * Copyright (C) 2023-2025 ViaVersion and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.viaversion.viafabricplus.injection.mixin.features.block.shape;
+
+import com.viaversion.viafabricplus.protocoltranslator.ProtocolTranslator;
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RedStoneWireBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(RedStoneWireBlock.class)
+public abstract class MixinRedStoneWireBlock extends Block {
+
+    @Unique
+    private final VoxelShape viaFabricPlus$outline_shape_r1_8_x = Shapes.box(0.0F, 0.0F, 0.0F, 1.0F, 0.0625F, 1.0F);
+
+    public MixinRedStoneWireBlock(final Properties properties) {
+        super(properties);
+    }
+
+    @Inject(method = "getShape", at = @At("HEAD"), cancellable = true)
+    private void changeOutlineShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext, CallbackInfoReturnable<VoxelShape> cir) {
+        if (ProtocolTranslator.getTargetVersion().olderThanOrEqualTo(ProtocolVersion.v1_8)) {
+            cir.setReturnValue(viaFabricPlus$outline_shape_r1_8_x);
+        }
+    }
+
+}
