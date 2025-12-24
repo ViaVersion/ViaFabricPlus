bff03e65e4fe6e2bdfe1422bd59421e506050f97
Exterminate <Exterminate5573@gmail.com>
2025-12-25 10:52:56 +1100

    Cleaned

--- src/main/java/com/viaversion/viafabricplus/injection/mixin/features/bedrock/block/MixinBambooStalkBlock.java
+++ src/main/java/com/viaversion/viafabricplus/injection/mixin/features/bedrock/block/MixinBambooStalkBlock.java
@@ -40,20 +40,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class MixinBambooStalkBlock {
 
     @Unique
-    private static final VoxelShape  viaFabricPlus$shape_small_bedrock = Block.box(8D, 0.0D, 8D, 10D, 16.0D, 10D);
+    private static final VoxelShape viaFabricPlus$shape_small_bedrock = Block.box(8D, 0.0D, 8D, 10D, 16.0D, 10D);
     @Unique
     private static final VoxelShape viaFabricPlus$shape_large_bedrock = Block.box(8D, 0.0D, 8D, 11D, 16.0D, 11D);
 
-    @Inject(method = "getShape", at = @At("HEAD"), cancellable = true)
+    @Inject(method = {"getShape", "getCollisionShape"}, at = @At("HEAD"), cancellable = true)
     private void fixBambooShape(BlockState blockState, BlockGetter world, BlockPos blockPos, CollisionContext context, CallbackInfoReturnable<VoxelShape> cir) {
-        if (ProtocolTranslator.getTargetVersion().equalTo(BedrockProtocolVersion.bedrockLatest)) {
-            VoxelShape voxelShape = blockState.getValue(BambooStalkBlock.AGE) == BambooStalkBlock.AGE_THICK_BAMBOO ? viaFabricPlus$shape_large_bedrock : viaFabricPlus$shape_small_bedrock;
-            cir.setReturnValue(voxelShape.move(blockState.getOffset(blockPos)));
-        }
-    }
-
-    @Inject(method = "getCollisionShape", at = @At("HEAD"), cancellable = true)
-    private void fixBambooCollisionShape(BlockState blockState, BlockGetter world, BlockPos blockPos, CollisionContext context, CallbackInfoReturnable<VoxelShape> cir) {
         if (ProtocolTranslator.getTargetVersion().equalTo(BedrockProtocolVersion.bedrockLatest)) {
             VoxelShape voxelShape = blockState.getValue(BambooStalkBlock.AGE) == BambooStalkBlock.AGE_THICK_BAMBOO ? viaFabricPlus$shape_large_bedrock : viaFabricPlus$shape_small_bedrock;
             cir.setReturnValue(voxelShape.move(blockState.getOffset(blockPos)));
