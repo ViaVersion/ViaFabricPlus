51dbec59abf4205cebe27c82a33448fff668908c
FlorianMichael <git@florianmichael.de>
2025-12-26 21:30:47 +0100

    Setup unobfuscated environment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
     }
 
     plugins {
-        id("net.fabricmc.fabric-loom-remap") version "1.15-SNAPSHOT"
+        id("net.fabricmc.fabric-loom") version "1.15-SNAPSHOT"
         id("de.florianreuth.baseproject") version "2.0.1"
     }
 }
