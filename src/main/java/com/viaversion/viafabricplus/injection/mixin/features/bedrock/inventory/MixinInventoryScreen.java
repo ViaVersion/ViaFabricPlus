80297f38414003c3d0429b0f848663d893572fa6
Exterminate <Exterminate5573@gmail.com>
2026-01-08 13:17:49 +1100

    Send Open Inventory Packet

---
+++ src/main/java/com/viaversion/viafabricplus/injection/mixin/features/bedrock/inventory/MixinInventoryScreen.java
@@ -0,0 +1,60 @@
+/*
+ * This file is part of ViaFabricPlus - https://github.com/ViaVersion/ViaFabricPlus
+ * Copyright (C) 2021-2026 the original authors
+ *                         - FlorianMichael/EnZaXD <git@florianmichael.de>
+ *                         - RK_01/RaphiMC
+ * Copyright (C) 2023-2026 ViaVersion and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.viaversion.viafabricplus.injection.mixin.features.bedrock.inventory;
+
+import com.viaversion.viafabricplus.ViaFabricPlusImpl;
+import com.viaversion.viafabricplus.protocoltranslator.ProtocolTranslator;
+import com.viaversion.viaversion.api.protocol.packet.PacketWrapper;
+import com.viaversion.viaversion.api.type.Types;
+import net.minecraft.client.gui.screens.inventory.InventoryScreen;
+import net.raphimc.viabedrock.api.BedrockProtocolVersion;
+import net.raphimc.viabedrock.protocol.BedrockProtocol;
+import net.raphimc.viabedrock.protocol.ServerboundBedrockPackets;
+import net.raphimc.viabedrock.protocol.data.enums.bedrock.generated.InteractPacket_Action;
+import net.raphimc.viabedrock.protocol.storage.EntityTracker;
+import net.raphimc.viabedrock.protocol.types.BedrockTypes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InventoryScreen.class)
+public class MixinInventoryScreen {
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void sendBedrockPacket(CallbackInfo ci) {
+        if (ProtocolTranslator.getTargetVersion().equalTo(BedrockProtocolVersion.bedrockLatest)) {
+            EntityTracker entityTracker = ProtocolTranslator.getPlayNetworkUserConnection().get(EntityTracker.class);
+            if (entityTracker == null) {
+                ViaFabricPlusImpl.INSTANCE.getLogger().error("Could not get bedrock entity tracker");
+                return;
+            }
+
+            final PacketWrapper interact = PacketWrapper.create(ServerboundBedrockPackets.INTERACT, ProtocolTranslator.getPlayNetworkUserConnection());
+            interact.write(Types.UNSIGNED_BYTE, (short) InteractPacket_Action.OpenInventory.getValue()); // action
+            interact.write(BedrockTypes.UNSIGNED_VAR_LONG, entityTracker.getClientPlayer().runtimeId()); // target entity runtime id
+            interact.write(BedrockTypes.OPTIONAL_POSITION_3F, null); // position
+            interact.sendToServer(BedrockProtocol.class);
+        }
+    }
+
+}
