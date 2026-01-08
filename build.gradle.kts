f26a6c1a86730bae8fe35f9f07743fe442ef24e5
FlorianMichael <git@florianmichael.de>
2026-01-08 22:09:34 +0100

    ViaFabricPlus 4.4.2
    Signed-off-by: FlorianMichael <git@florianmichael.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     testImplementation("net.fabricmc:fabric-loader-junit:${property("fabric_loader_version")}")
     modCompileOnly("com.terraformersmc:modmenu:15.0.0")
 
-    jij("net.raphimc:MinecraftAuth:5.0.1-SNAPSHOT") {
+    jij("net.raphimc:MinecraftAuth:5.0.1-20251223.202750-3") {
         exclude(group = "com.google.code.gson", module = "gson")
     }
     jij("net.lenni0451:Reflect:1.6.1")
@@ -76,15 +76,15 @@ includeTransitiveJijDependencies()
 
 fun Project.configureVVDependencies(configuration: String) {
     dependencies {
-        configuration("com.viaversion:viaversion-common:5.7.1-SNAPSHOT")
-        configuration("com.viaversion:viabackwards-common:5.7.1-SNAPSHOT")
-        configuration("com.viaversion:viaaprilfools-common:4.0.7")
-        configuration("com.viaversion:vialoader:4.0.6") {
+        configuration("com.viaversion:viaversion-common:5.7.1-20260107.151814-3")
+        configuration("com.viaversion:viabackwards-common:5.7.1-20260107.162814-6")
+        configuration("com.viaversion:viaaprilfools-common:4.0.8-20260104.120511-2")
+        configuration("com.viaversion:vialoader:4.0.7-20260105.151613-7") {
             exclude(group = "com.google.guava", module = "guava")
             exclude(group = "org.slf4j", module = "slf4j-api")
         }
-        configuration("net.raphimc:ViaLegacy:3.0.13-SNAPSHOT")
-        configuration("net.raphimc:ViaBedrock:0.0.25-SNAPSHOT") {
+        configuration("net.raphimc:ViaLegacy:3.0.13-20260106.005210-4")
+        configuration("net.raphimc:ViaBedrock:0.0.25-20260102.185758-2") {
             exclude(group = "com.mojang", module = "brigadier")
             exclude(group = "at.yawk.lz4", module = "lz4-java")
             exclude(group = "io.netty")
