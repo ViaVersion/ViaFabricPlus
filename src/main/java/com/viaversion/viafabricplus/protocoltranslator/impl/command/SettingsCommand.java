07d814ae9e6fe1d349045a96b8892983e09012da
lowercasebtw <126462578+lowercasebtw@users.noreply.github.com>
2026-01-15 14:18:22 -0500

    Add settings subcommand (#1035)
    Co-authored-by: lowercasebtw <jejojora2@gmail.com>
    (cherry picked from commit 5ddc4e40fa5a1dd9e7b91101345fe1310d627ade)

---
+++ src/main/java/com/viaversion/viafabricplus/protocoltranslator/impl/command/SettingsCommand.java
@@ -0,0 +1,45 @@
+/*
+ * This file is part of ViaFabricPlus - https://github.com/ViaVersion/ViaFabricPlus
+ * Copyright (C) 2021-2026 the original authors
+ *                         - FlorianMichael/EnZaXD <git@florianmichael.de>
+ *                         - RK_01/RaphiMC
+ * Copyright (C) 2023-2026 ViaVersion and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.viaversion.viafabricplus.protocoltranslator.impl.command;
+
+import com.viaversion.viafabricplus.screen.impl.SettingsScreen;
+import com.viaversion.viaversion.api.command.ViaCommandSender;
+
+public final class SettingsCommand implements VFPViaSubCommand {
+
+    @Override
+    public String name() {
+        return "settings";
+    }
+
+    @Override
+    public String description() {
+        return "Opens ViaFabricPlus's configuration/options screen";
+    }
+
+    @Override
+    public boolean execute(ViaCommandSender sender, String[] args) {
+        SettingsScreen.INSTANCE.open(null);
+        return true;
+    }
+
+}
