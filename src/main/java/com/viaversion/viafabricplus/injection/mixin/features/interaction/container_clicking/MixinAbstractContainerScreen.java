9f3a0f0396950cdb0e3b8f461903a5d18cab6786
lowercasebtw <jejojora2@gmail.com>
2026-01-16 16:27:20 -0500

    Fix mouse inventory interactions in <=1.5.x

--- src/main/java/com/viaversion/viafabricplus/injection/mixin/features/interaction/container_clicking/MixinAbstractContainerScreen.java
+++ src/main/java/com/viaversion/viafabricplus/injection/mixin/features/interaction/container_clicking/MixinAbstractContainerScreen.java
@@ -21,25 +21,56 @@
 
 package com.viaversion.viafabricplus.injection.mixin.features.interaction.container_clicking;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.viaversion.viafabricplus.protocoltranslator.ProtocolTranslator;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.input.KeyEvent;
+import net.minecraft.client.input.MouseButtonEvent;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.inventory.ClickType;
+import net.minecraft.world.inventory.Slot;
 import net.raphimc.vialegacy.api.LegacyProtocolVersion;
+import org.jspecify.annotations.NonNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractContainerScreen.class)
-public abstract class MixinAbstractContainerScreen {
+public abstract class MixinAbstractContainerScreen extends Screen {
+
+    @Shadow
+    public abstract boolean mouseClicked(final @NonNull MouseButtonEvent mouseButtonEvent, final boolean bl);
+
+    protected MixinAbstractContainerScreen(final Component component) {
+        super(component);
+    }
+
+    @Redirect(method = {"mouseClicked", "mouseReleased"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/input/MouseButtonEvent;hasShiftDown()Z"))
+    private boolean disableShiftClickItems(final MouseButtonEvent instance) {
+        return instance.hasShiftDown() && ProtocolTranslator.getTargetVersion().newerThan(LegacyProtocolVersion.r1_5_2);
+    }
+
+    // TODO: Item is supposed to go in slot on click not where mouse is released
+    @Inject(method = "mouseDragged", at = @At("HEAD"), cancellable = true)
+    private void disableItemDragging(final MouseButtonEvent mouseButtonEvent, final double d, final double e, final CallbackInfoReturnable<Boolean> cir) {
+        if (ProtocolTranslator.getTargetVersion().olderThanOrEqualTo(LegacyProtocolVersion.r1_4_6tor1_4_7)) {
+            cir.setReturnValue(super.mouseDragged(mouseButtonEvent, d, e));
+        }
+    }
+
+    @WrapWithCondition(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/inventory/AbstractContainerScreen;slotClicked(Lnet/minecraft/world/inventory/Slot;IILnet/minecraft/world/inventory/ClickType;)V", ordinal = 0))
+    private boolean disableItemCloning(final AbstractContainerScreen<?> instance, final Slot slot, final int slotIndex, final int count, final ClickType clickType) {
+        return ProtocolTranslator.getTargetVersion().newerThan(LegacyProtocolVersion.r1_3_1tor1_3_2);
+    }
 
     @Redirect(method = "checkHotbarKeyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/KeyMapping;matches(Lnet/minecraft/client/input/KeyEvent;)Z", ordinal = 1))
     private boolean disableHotbarKeys(KeyMapping instance, KeyEvent keyEvent) {
-        if (ProtocolTranslator.getTargetVersion().olderThanOrEqualTo(LegacyProtocolVersion.r1_3_1tor1_3_2)) {
-            return false;
-        } else {
-            return instance.matches(keyEvent);
-        }
+        return instance.matches(keyEvent) && ProtocolTranslator.getTargetVersion().newerThan(LegacyProtocolVersion.r1_3_1tor1_3_2);
     }
 
 }
