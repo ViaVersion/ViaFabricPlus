a4b1847962ff7b31fe247f4b3f6254634e14d630
Florian Reuth <git@florianreuth.de>
2026-02-07 16:21:59 +0100

    Explicitly require fabric loom plugin
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- viafabricplus-visuals/build.gradle.kts
+++ viafabricplus-visuals/build.gradle.kts
@@ -1,5 +1,9 @@
 import de.florianreuth.baseproject.loadFabricApiModules
 
+plugins {
+    id("net.fabricmc.fabric-loom-remap")
+}
+
 dependencies {
     loadFabricApiModules("fabric-lifecycle-events-v1")
     compileOnly(project(":viafabricplus-api"))
