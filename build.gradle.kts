12ae8ea963efe9b561b63c238d1ba398e61d3d0c
Florian Reuth <git@florianreuth.de>
2026-03-02 19:08:33 +0100

    Start 4.4.9 cycle; Update ViaVersion
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,7 +77,7 @@ fun configureBedrockDependencies() {
 
 fun Project.configureVVDependencies(configuration: String) {
     dependencies {
-        configuration("com.viaversion:viaversion-common:5.7.2")
+        configuration("com.viaversion:viaversion-common:5.7.3-SNAPSHOT")
         configuration("com.viaversion:viabackwards-common:5.7.2")
         configuration("com.viaversion:viaaprilfools-common:4.0.9")
         configuration("net.raphimc:ViaLegacy:3.0.14")
