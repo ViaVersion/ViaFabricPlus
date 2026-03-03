fcc8e8bf7e8142634d7c2afeeadd361577855181
Florian Reuth <git@florianreuth.de>
2026-03-03 14:42:01 +0100

    Add back missing mixin
    Fixes https://github.com/ViaVersion/ViaFabricPlus/issues/1075
    
    Signed-off-by: Florian Reuth <git@florianreuth.de>

---
+++ src/main/java/com/viaversion/viafabricplus/injection/mixin/base/access/MixinExtensionProtocolMetadataStorage.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of ViaFabricPlus - https://github.com/ViaVersion/ViaFabricPlus
+ * Copyright (C) 2021-2026 the original authors
+ *                         - Florian Reuth <git@florianreuth.de>
+ *                         - RK_01/RaphiMC
+ * Copyright (C) 2023-2026 ViaVersion and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.viaversion.viafabricplus.injection.mixin.base.access;
+
+import com.viaversion.viafabricplus.injection.access.base.IExtensionProtocolMetadataStorage;
+import java.util.EnumMap;
+import net.raphimc.vialegacy.protocol.classic.c0_30cpetoc0_28_30.data.ClassicProtocolExtension;
+import net.raphimc.vialegacy.protocol.classic.c0_30cpetoc0_28_30.storage.ExtensionProtocolMetadataStorage;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(value = ExtensionProtocolMetadataStorage.class, remap = false)
+public abstract class MixinExtensionProtocolMetadataStorage implements IExtensionProtocolMetadataStorage {
+
+    @Shadow
+    @Final
+    private EnumMap<ClassicProtocolExtension, Integer> serverExtensions;
+
+    @Override
+    public EnumMap<ClassicProtocolExtension, Integer> viaFabricPlus$getServerExtensions() {
+        return this.serverExtensions;
+    }
+
+}
