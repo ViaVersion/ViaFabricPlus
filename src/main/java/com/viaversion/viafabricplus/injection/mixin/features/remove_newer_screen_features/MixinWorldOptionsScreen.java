479c81c008ba0b97f373f0a55544213d435ca1a9
Florian Reuth <git@florianreuth.de>
2026-04-01 22:40:38 +0200

    Disable game rules button in <= 1.21.11
    Signed-off-by: Florian Reuth <git@florianreuth.de>

---
+++ src/main/java/com/viaversion/viafabricplus/injection/mixin/features/remove_newer_screen_features/MixinWorldOptionsScreen.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of ViaFabricPlus - https://github.com/ViaVersion/ViaFabricPlus
+ * Copyright (C) 2021-2026 the original authors
+ *                         - Florian Reuth <git@florianreuth.de>
+ *                         - RK_01/RaphiMC
+ * Copyright (C) 2023-2026 ViaVersion and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.viaversion.viafabricplus.injection.mixin.features.remove_newer_screen_features;
+
+import com.viaversion.viafabricplus.protocoltranslator.ProtocolTranslator;
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.options.WorldOptionsScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(WorldOptionsScreen.class)
+public abstract class MixinWorldOptionsScreen {
+
+    @Inject(method = "createGameRulesButton", at = @At("RETURN"))
+    private void disableGameRules(CallbackInfoReturnable<Button> cir) {
+        if (ProtocolTranslator.getTargetVersion().olderThanOrEqualTo(ProtocolVersion.v1_21_11)) {
+            cir.getReturnValue().active = false;
+        }
+    }
+
+}
