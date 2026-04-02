1699a915588100e9e204c2972a0f1326d7f3c34e
Kichura <68134602+Kichura@users.noreply.github.com>
2026-04-03 00:44:06 +0200

    VV 5.9.0-SNAPSHOT for 1.21.11.

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,10 +77,10 @@ fun configureBedrockDependencies() {
 
 fun Project.configureVVDependencies(configuration: String) {
     dependencies {
-        configuration("com.viaversion:viaversion-common:5.8.1")
-        configuration("com.viaversion:viabackwards-common:5.8.1")
-        configuration("com.viaversion:viaaprilfools-common:4.1.0")
-        configuration("net.raphimc:ViaLegacy:3.0.14")
+        configuration("com.viaversion:viaversion-common:5.9.0-SNAPSHOT")
+        configuration("com.viaversion:viabackwards-common:5.9.0-SNAPSHOT")
+        configuration("com.viaversion:viaaprilfools-common:4.2.0-SNAPSHOT")
+        configuration("net.raphimc:ViaLegacy:3.0.15-SNAPSHOT")
         configuration("net.raphimc:ViaBedrock:0.0.27-20260329.200308-2") {
             exclude(group = "com.mojang", module = "brigadier")
             exclude(group = "at.yawk.lz4", module = "lz4-java")
