5f947812e40eb871103bdfa3b8b21c3423051285
Florian Reuth <git@florianreuth.de>
2026-06-23 01:32:34 +0200

    Add new ones as well
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- src/main/java/com/viaversion/viafabricplus/protocoltranslator/protocol/ViaFabricPlusProtocol.java
+++ src/main/java/com/viaversion/viafabricplus/protocoltranslator/protocol/ViaFabricPlusProtocol.java
@@ -78,6 +78,8 @@ public final class ViaFabricPlusProtocol extends AbstractProtocol<ClientboundPac
         ITEM_TYPES.put(ProtocolVersion.v1_21_7, new ItemTypes(VersionedTypes.V1_21_6.item, VersionedTypes.V1_21_6.lengthPrefixedItem));
         ITEM_TYPES.put(ProtocolVersion.v1_21_9, new ItemTypes(VersionedTypes.V1_21_9.item, VersionedTypes.V1_21_9.lengthPrefixedItem));
         ITEM_TYPES.put(ProtocolVersion.v1_21_11, new ItemTypes(VersionedTypes.V1_21_11.item, VersionedTypes.V1_21_11.lengthPrefixedItem));
+        ITEM_TYPES.put(ProtocolVersion.v26_1, new ItemTypes(VersionedTypes.V26_1.item, VersionedTypes.V26_1.lengthPrefixedItem));
+        ITEM_TYPES.put(ProtocolVersion.v26_2, new ItemTypes(VersionedTypes.V26_2.item, VersionedTypes.V26_2.lengthPrefixedItem));
 
         if (!ITEM_TYPES.containsKey(ProtocolTranslator.NATIVE_VERSION)) {
             throw new IllegalStateException("Missing item type for native version");
