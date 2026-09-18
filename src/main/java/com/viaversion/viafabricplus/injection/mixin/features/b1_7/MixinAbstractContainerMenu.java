777523e41fa5508b1d4938859b1d32b6702b1126
Evelyn <36399055+Socketlike@users.noreply.github.com>
2026-09-18 19:43:30 +0700

    new mixin attempting to disable max craft (shift clicking on output) on below b1.6 and b1.6.5 to below b1.8

---
+++ src/main/java/com/viaversion/viafabricplus/injection/mixin/features/b1_7/MixinAbstractContainerMenu.java
@@ -0,0 +1,48 @@
+/*
+ * This file is part of ViaFabricPlus - https://github.com/ViaVersion/ViaFabricPlus
+ * Copyright (C) 2021-2026 the original authors
+ *                         - Florian Reuth <git@florianreuth.de>
+ *                         - RK_01/RaphiMC
+ * Copyright (C) 2023-2026 ViaVersion and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.viaversion.viafabricplus.injection.mixin.features.b1_7;
+
+import com.viaversion.viafabricplus.ViaFabricPlus;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.AbstractCraftingMenu;
+import net.minecraft.world.item.ItemStack;
+import net.raphimc.vialegacy.api.LegacyProtocolVersion;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(AbstractContainerMenu.class)
+public abstract class MixinAbstractContainerMenu {
+
+    @Redirect(method = "doClick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/AbstractContainerMenu;quickMoveStack(Lnet/minecraft/world/entity/player/Player;I)Lnet/minecraft/world/item/ItemStack;", ordinal = 0))
+    private ItemStack disableQuickCraftMaxAmount(AbstractContainerMenu instance, Player player, int slotIndex) {
+        ItemStack clicked = instance.quickMoveStack(player, slotIndex);
+
+        if ((Object) this instanceof AbstractCraftingMenu && slotIndex == 0 && ViaFabricPlus.api().targetVersion().olderThanOrEqualTo(LegacyProtocolVersion.b1_7tob1_7_3)) {
+            return ItemStack.EMPTY;
+        }
+
+        return clicked;
+    }
+
+}
