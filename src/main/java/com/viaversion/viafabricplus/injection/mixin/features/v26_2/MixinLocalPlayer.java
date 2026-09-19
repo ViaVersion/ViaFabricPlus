0f8102ecf4fb12d7482722894c11f45d0a81c497
Florian Reuth <git@florianreuth.de>
2026-09-19 20:38:17 +0200

    Fix player pushing while riding
    Signed-off-by: Florian Reuth <git@florianreuth.de>

---
+++ src/main/java/com/viaversion/viafabricplus/injection/mixin/features/v26_2/MixinLocalPlayer.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of ViaFabricPlus - https://github.com/ViaVersion/ViaFabricPlus
+ * Copyright (C) 2021-2026 the original authors
+ *                         - Florian Reuth <git@florianreuth.de>
+ *                         - RK_01/RaphiMC
+ * Copyright (C) 2023-2026 ViaVersion and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.viaversion.viafabricplus.injection.mixin.features.v26_2;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.viaversion.viafabricplus.ViaFabricPlus;
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
+import net.minecraft.client.player.LocalPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(LocalPlayer.class)
+public abstract class MixinLocalPlayer {
+
+    @ModifyExpressionValue(method = "aiStep", at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/client/player/LocalPlayer;isPassenger()Z"))
+    private boolean moveOutOfBlocksWhileRiding(boolean original) {
+        // 26.3 no longer pushes the player out of blocks while riding
+        return ViaFabricPlus.api().targetVersion().newerThanOrEqualTo(ProtocolVersion.v26_3) && original;
+    }
+
+}
